a024665ff4256e5a46dd367146a22c25c1d6a6c8
shamanomania <slutskiy@phys.vsu.ru>
2016-12-24 16:54:55 +0300

    changed

---
+++ src/main/java/com/netcracker/HelloController.java
@@ -0,0 +1,14 @@
+package com.netcracker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, world!";
+    }
+    
+}
