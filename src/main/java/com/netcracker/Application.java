102c192922044df57f204ee0c29d2a4452e4a093
kravchenko-da <dmitrii.kravchenko@netcracker.com>
2016-12-24 20:09:22 +0300

    Removed deprecated usage Added suggestion for jdbc driver

--- src/main/java/com/netcracker/Application.java
+++ src/main/java/com/netcracker/Application.java
@@ -1,8 +1,8 @@
 package com.netcracker;
 
         import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.web.support.SpringBootServletInitializer;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.boot.context.web.SpringBootServletInitializer;
         import org.springframework.boot.builder.SpringApplicationBuilder;
         import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
