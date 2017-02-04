11aabbfb8aafe9b2b13396ec459e501ec099f036
shamanomania <slutskiy@phys.vsu.ru>
2017-02-04 18:27:05 +0300

    zamena findByName na findByTitle

--- persistence/src/main/java/netcracker/repository/UserRepository.java
+++ persistence/src/main/java/netcracker/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<UserTest, Long> {
-    List<UserTest> findByName(String name);
+    List<UserTest> findByTitle(String name);
 }
