70e9e535d40116e8a7547ed06598cb9cf533f7d1
sivko <sivkostja@gmail.com>
2017-04-03 11:30:49 +0300

    Added oath with Google, Facebook with Spring Social

--- src/main/java/netcracker/controllers/GoogleController.java
+++ src/main/java/netcracker/controllers/GoogleController.java
@@ -3,23 +3,19 @@ package netcracker.controllers;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 /**
- * Created by Sid775 on 10.03.2017.
+ * Created by sivko on 02.04.2017.
  */
+
 @Controller
-public class HomeController {
-
-    @RequestMapping("/")
-    public String getHomePage() {
-        return "home";
+@EnableOAuth2Sso
+public class GoogleController {
+    @RequestMapping("/goInt")
+    public String GoogleAp() {
+        return "goInt";
     }
-
-    @RequestMapping("/ide")
-    public String getIDEPage(){
-        return "ide";
-    }
-
 }
